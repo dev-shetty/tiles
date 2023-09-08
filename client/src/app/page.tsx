7ec81d583b4925a2ad9418f83d532e564d2358c3
Deveesh Shetty <deveeshshetty@gmail.com>
2023-09-08 12:51:15 +0530

    Initial Frontend Setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from "@/components/Home"
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Home />
+    </main>
+  )
+}
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,5 @@
+interface HomeProps {}
+
+export default function Home({}: HomeProps) {
+  return <div>Hello World</div>
+}
