b914d2fa715f20c9334e7a829dc28b542061bbbe
Deveesh Shetty <deveeshshetty@gmail.com>
2023-09-15 20:19:35 +0530

    adds meta info

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -8,7 +8,38 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Tiles",
-  description: "Tiles and Fun",
+  description: "Place a tile and contribute to the canvas!",
+  authors: [
+    {
+      name: "Deveesh Shetty",
+      url: "deveesh.vercel.app",
+    },
+  ],
+  creator: "Deveesh Shetty",
+  keywords: ["tiles", "canvas", "pixel", "art", "drawing"],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://placetile.vercel.app",
+    title: "Tiles",
+    description: "Place a tile and contribute to the canvas!",
+    siteName: "Tiles",
+    images: [
+      {
+        url: "https://placetile.vercel.app/og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Tiles",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tiles",
+    description: "Place a tile and contribute to the canvas!",
+    images: ["https://placetile.vercel.app/og.jpg"],
+    creator: "@ShettyDeveesh",
+  },
 }
 
 export default function RootLayout({
